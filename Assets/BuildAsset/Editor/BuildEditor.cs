8a9cc7685322f869553a94ab42839024246a2318
botf <victor.mendele68@gmail.com>
2017-07-24 20:42:01 +0200

    Refactoring

---
+++ Assets/BuildAsset/Editor/BuildEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Build))]
+public class BuildEditor : Editor
+{
+	public override void OnInspectorGUI ()
+	{
+		Build build = (Build)target;
+
+		build.ApplyBuildHandlerScale = EditorGUILayout.Toggle("Apply BuildHandler scale", build.ApplyBuildHandlerScale);
+		build.UseRandomColor = EditorGUILayout.Toggle("Use random color", build.UseRandomColor);
+
+		build.Size = EditorGUILayout.Vector3Field("Build collider size", build.Size);
+
+	}
+}
---
+++ Assets/BuildAsset/Editor/BuildHandlerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BuildHandler))]
+public class BuildHandlerEditor : Editor {
+	
+	public override void OnInspectorGUI ()
+	{
+		BuildHandler buildHandler = (BuildHandler) target;
+
+		buildHandler.GridShowMod = (BuildHandler.BuildGridShowMod)EditorGUILayout.EnumPopup("Grid show mod", buildHandler.GridShowMod);
+		if (buildHandler.GridShowMod == BuildHandler.BuildGridShowMod.Gizmos)
+		{
+			buildHandler.GizmosGridColor = EditorGUILayout.ColorField("Gizmos color", buildHandler.GizmosGridColor);
+		}
+
+		buildHandler.UseSecureSizes = EditorGUILayout.Toggle("Use secure size", buildHandler.UseSecureSizes);
+
+		buildHandler.SpacingCountX = EditorGUILayout.IntField("Spacing count X", buildHandler.SpacingCountX);
+		buildHandler.SpacingCountY = EditorGUILayout.IntField("Spacing count Y", buildHandler.SpacingCountY);
+	}
+}
